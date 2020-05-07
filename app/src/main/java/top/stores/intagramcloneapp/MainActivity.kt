55d122127a46a93ded72dae26b0b602312303298
madubashena89 <madubashena89@gmail.com>
2020-05-06 22:57:10 -0500

    fragments added

--- app/src/main/java/top/stores/intagramcloneapp/MainActivity.kt
+++ app/src/main/java/top/stores/intagramcloneapp/MainActivity.kt
@@ -4,47 +4,57 @@ import android.os.Bundle
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import top.stores.intagramcloneapp.Fragments.HomeFragment
+import top.stores.intagramcloneapp.Fragments.NotificationsFragment
+import top.stores.intagramcloneapp.Fragments.ProfileFragment
+import top.stores.intagramcloneapp.Fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView :TextView
+    internal var selectedFragment : Fragment? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        textView = findViewById(R.id.message)
 
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container, HomeFragment()
+        ).commit()
     }
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                textView.setText("Home")
-                return@OnNavigationItemSelectedListener true
+                 selectedFragment = HomeFragment()
             }
             R.id.nav_search -> {
-                textView.setText("Search")
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = SearchFragment()
             }
             R.id.nav_add_post -> {
-                textView.setText("Add Post")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                textView.setText("Notifications")
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = NotificationsFragment()
             }
             R.id.nav_profile -> {
-                textView.setText("Profile")
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = ProfileFragment()
             }
+        }
+
+        if(selectedFragment != null){
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container, selectedFragment!!
+            ).commit()
         }
 
         false
